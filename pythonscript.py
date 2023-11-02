f1feccba99900d6e93dfb6d874bc7b08b52fcf4f
ahmedaazri <76480820+ahmedaazri@users.noreply.github.com>
2023-11-02 02:04:36 +0100

    Update pythonscript.py

--- pythonscript.py
+++ pythonscript.py
@@ -189,7 +189,9 @@ with input_col:
 # Check if r is within the [d, u] interval
 dt = T / n
 u = m.exp(v * m.sqrt(dt))
+print(u)
 d = 1 / u
+print(d)
 
 if r < d or r > u:
     st.write("Erreur : Existence d'opportunité d'arbitrage. Le taux d'intérêt (r) doit être dans l'intervalle [d, u].")
